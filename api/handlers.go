7015ae76c8a3a4807d12a4c8e109404677139cd0
jerry-enebeli <jerryenebeli@gmail.com>
2022-12-16 15:22:55 +0100

    update readme

--- api/handlers.go
+++ api/handlers.go
@@ -1,33 +1,7 @@
 package api
 
+import "github.com/jerry-enebeli/saifu"
+
 type API struct {
-	wallet wallets.Wallet
-}
-
-func (API) DebitWallet() {
-	panic("implement me")
-}
-
-func (API) CreditWallet() {
-	panic("implement me")
-}
-
-func (API) CreateWallet() {
-	panic("implement me")
-}
-
-func (API) GetWallet() {
-	panic("implement me")
-}
-
-func (API) GetWallets() {
-	panic("implement me")
-}
-
-func (API) UpdateWallet() {
-	panic("implement me")
-}
-
-func (API) DeleteWallet() {
-	panic("implement me")
+	ledger saifu.Ledger
 }
