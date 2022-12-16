760f05847fef0af7c0f83cd2d6b611e400748591
jerry-enebeli <jerryenebeli@gmail.com>
2022-12-16 15:21:07 +0100

    update readme

--- datasources/datasoruce.go
+++ datasources/datasoruce.go
@@ -3,17 +3,10 @@ package datasources
 import (
 	"log"
 
-	"github.com/jerry-enebeli/saifu/model"
-
 	"github.com/jerry-enebeli/saifu/config"
 )
 
 type DataSource interface {
-	CreateWallet() (model.Wallet, error)
-	GetWalletByIdentifier(identifier model.WalletIdentifier) (model.Wallet, error)
-	GetAllWallets(filter model.WalletFilter) ([]model.Wallet, error)
-	UpdateWallet(identifier model.WalletIdentifier, wallet model.Wallet) ([]model.Wallet, error)
-	DeleteWallet(identifier model.WalletIdentifier) ([]model.Wallet, error)
 }
 
 func NewDataSource(configuration *config.Configuration) DataSource {
