a38fbadadb379566302a10125ff269a07053d3c2
jerry-enebeli <jerryenebeli@gmail.com>
2024-07-26 07:58:07 +0100

    add post ledger actions to index data after creation

--- ledger.go
+++ ledger.go
@@ -1,9 +1,32 @@
 package blnk
 
-import "github.com/jerry-enebeli/blnk/model"
+import (
+	"context"
+
+	"github.com/jerry-enebeli/blnk/internal/notification"
+	"github.com/jerry-enebeli/blnk/model"
+)
+
+func (l *Blnk) postLedgerActions(_ context.Context, ledger *model.Ledger) {
+	go func() {
+		l.queue.queueIndexData(ledger.LedgerID, "ledgers", ledger)
+		err := SendWebhook(NewWebhook{
+			Event:   "ledger.created",
+			Payload: ledger,
+		})
+		if err != nil {
+			notification.NotifyError(err)
+		}
+	}()
+}
 
 func (l *Blnk) CreateLedger(ledger model.Ledger) (model.Ledger, error) {
-	return l.datasource.CreateLedger(ledger)
+	ledger, err := l.datasource.CreateLedger(ledger)
+	if err != nil {
+		return model.Ledger{}, err
+	}
+	l.postLedgerActions(context.Background(), &ledger)
+	return ledger, nil
 }
 
 func (l *Blnk) GetAllLedgers() ([]model.Ledger, error) {
