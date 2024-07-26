2cbbd6e13c78cdaf01b32c5b265de7a65200e430
jerry-enebeli <jerryenebeli@gmail.com>
2024-07-26 08:01:17 +0100

    Add INDEX_QUEUE constant and queueIndexData function to enqueue index data

--- queue.go
+++ queue.go
@@ -16,6 +16,7 @@ import (
 
 const TRANSACTION_QUEUE = "new:transaction"
 const WEBHOOK_QUEUE = "new:webhoook"
+const INDEX_QUEUE = "new:index"
 const EXPIREDINFLIGHT_QUEUE = "new:inflight-expiry"
 const NumberOfQueues = 20
 
@@ -52,6 +53,28 @@ func (q *Queue) queueInflightExpiry(transactionID string, expiresAt time.Time) e
 	return nil
 }
 
+func (q *Queue) queueIndexData(id string, collection string, data interface{}) error {
+	payload := map[string]interface{}{
+		"collection": collection,
+		"payload":    data,
+	}
+
+	IPayload, err := json.Marshal(payload)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	taskOptions := []asynq.Option{asynq.TaskID(id), asynq.Queue(INDEX_QUEUE)}
+	task := asynq.NewTask(INDEX_QUEUE, IPayload, taskOptions...)
+	info, err := q.Client.Enqueue(task)
+	if err != nil {
+		log.Println(err, info)
+		return err
+	}
+	log.Printf(" [*] Successfully enqueued index data: %+v", id)
+	return nil
+}
+
 func (q *Queue) Enqueue(_ context.Context, transaction *model.Transaction) error {
 	payload, err := json.Marshal(transaction)
 	if err != nil {
@@ -77,7 +100,7 @@ func (q *Queue) geTask(transaction *model.Transaction, payload []byte) *asynq.Ta
 	queueIndex := hashBalanceID(transaction.Source) % NumberOfQueues
 	queueName := fmt.Sprintf("%s_%d", TRANSACTION_QUEUE, queueIndex+1) // Queue names are 1-based
 
-	taskOptions := []asynq.Option{asynq.TaskID(transaction.Reference), asynq.Queue(queueName)}
+	taskOptions := []asynq.Option{asynq.TaskID(transaction.TransactionID), asynq.Queue(queueName)}
 
 	if !transaction.ScheduledFor.IsZero() {
 		taskOptions = append(taskOptions, asynq.ProcessIn(time.Until(transaction.ScheduledFor)))
@@ -90,4 +113,20 @@ func hashBalanceID(balanceID string) int {
 	hasher := fnv.New32a()
 	_, _ = hasher.Write([]byte(balanceID))
 	return int(hasher.Sum32())
+}
+
+func (q *Queue) GetTransactionFromQueue(transactionID string) (*model.Transaction, error) {
+	// Iterate over all specific transaction queues
+	for i := 1; i <= NumberOfQueues; i++ {
+		queueName := fmt.Sprintf("%s_%d", TRANSACTION_QUEUE, i)
+		task, err := q.Inspector.GetTaskInfo(queueName, transactionID)
+		if err == nil && task != nil {
+			var txn model.Transaction
+			if err := json.Unmarshal(task.Payload, &txn); err != nil {
+				return nil, err
+			}
+			return &txn, nil
+		}
+	}
+	return nil, nil // Return nil if transaction is not found in any queue
 }
