360693417cada41828310f1265fab2021dfd3ef3
jerry-enebeli <jerryenebeli@gmail.com>
2024-09-10 06:20:18 +0100

    cleanup

--- sql/1712749737.sql
+++ sql/1712749737.sql
@@ -1,3 +1,17 @@
+-- Copyright 2024 Blnk Finance Authors.
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+
 -- +migrate Up
 create or replace function blnk.get_balances_by_id(source_id TEXT, destination_id TEXT)
     returns table(balance_id TEXT, currency TEXT, currency_multiplier BIGINT,ledger_id TEXT, balance BIGINT, credit_balance BIGINT, debit_balance BIGINT, inflight_balance BIGINT, inflight_credit_balance BIGINT, inflight_debit_balance BIGINT, created_at TIMESTAMP, version INTEGER)
--- sql/1713790634.sql
+++ sql/1713790634.sql
@@ -1,3 +1,17 @@
+-- Copyright 2024 Blnk Finance Authors.
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+
 -- +migrate Up
 ALTER TABLE blnk.balance_monitors DROP CONSTRAINT IF EXISTS balance_monitors_field_check;
 ALTER TABLE blnk.balance_monitors ADD CONSTRAINT balance_monitors_field_check CHECK (field IN ('debit_balance', 'credit_balance', 'balance', 'inflight_debit_balance', 'inflight_credit_balance', 'inflight_balance'));
